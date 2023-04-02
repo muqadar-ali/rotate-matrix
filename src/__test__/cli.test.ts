bf5cbbff92a2a72a013bf5951399c29e31de76c2
muqadar-ali <41318315+muqadar-ali@users.noreply.github.com>
2023-04-03 00:55:42 +0200

    update test cases

--- src/__test__/cli.test.ts
+++ src/__test__/cli.test.ts
@@ -2,8 +2,8 @@ import { Matrix } from "../matrix";
 
 describe('Matrix', () => {
     describe('rotateLeft', () => {
-      it('should rotate the matrix left', () => {
-        const matrix = new Matrix([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      it('should rotate the matrix left - 3x3 matrix', () => {
+        const matrix = new Matrix(Array.from({ length: 9}, (_, index) => index + 1));
         const result = matrix.rotateLeft();
   
         expect(result).toEqual({
@@ -11,9 +11,20 @@ describe('Matrix', () => {
           data: [2,3,6,1,5,9,4,7,8],
         });
       });
+
+      it('should rotate the matrix left - 4x4 matrix', () => {
+        const matrix = new Matrix(Array.from({ length: 16}, (_, index) => index + 1));
+        const result = matrix.rotateLeft();
+  
+        expect(result).toEqual({
+          isValid: true,
+          data: [2,3,4,8,1,3,11,12,5,6,10,16,9,13,14,15],
+        });
+      });
+  
   
       it('should return the empty matrix if it is invalid - odd', () => {
-        const matrix = new Matrix([1, 2, 3, 4, 5]);
+        const matrix = new Matrix(Array.from({ length: 5}, (_, index) => index + 1));
         const result = matrix.rotateLeft();
   
         expect(result).toEqual({
